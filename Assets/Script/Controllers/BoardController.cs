8ea54ac246914993086372d021c744000ffb8c1d
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-04-06 19:07:44 +0300

    BoardController

---
+++ Assets/Script/Controllers/BoardController.cs
@@ -0,0 +1,22 @@
+﻿using Script.GlobalManagers;
+using UnityEngine;
+
+namespace Script.Controllers
+{
+	public class BoardController : MonoBehaviour
+	{
+		[Header("Prefabs")]
+		[SerializeField] private GameObject _defaultBoard;
+		[SerializeField] private GameObject _player;
+		[SerializeField] private GameObject _dragon;
+		[SerializeField] private GameObject _treasureChest;
+		[SerializeField] private GameObject _bomb;
+		
+		private GameObject _board;
+
+		private void Awake()
+		{
+			_board = Instantiate(BoardManager.ChosenBoard ? BoardManager.ChosenBoard : _defaultBoard, transform);
+		}
+	}
+}
